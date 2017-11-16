b71f667d66f8079aa642a4e06b7911865b7c2930
Dani <danimb93@gmail.com>
2017-11-16 19:33:09 +0100

    Component delete y update avisos

--- src/app/Componentes/aviso/aviso.component.ts
+++ src/app/Componentes/aviso/aviso.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AvisoService } from '../../Services/aviso.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-aviso',
@@ -21,5 +22,18 @@ export class AvisoComponent implements OnInit {
       console.log(err);
     });
   }
-
+  deleteAviso(id){
+    this.avisoService.deleteAviso(id).then((result) => {
+        this.router.navigate(['avisos']);
+    }, (err) => {
+      console.log(err);
+    });
+  }
+  updateAviso(id){
+    this.avisoService.updateAviso(id, this.aviso).then((result) => {
+      let id = result['_id'];
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
