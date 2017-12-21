4ead14597db2d22ecd194e53de946442ced7ac73
Ben Kenobi <roberto.arranz.93@gmail.com>
2017-12-21 18:11:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  /*var allowedOrigins = ['http://127.0.0.1:8100', 'http://localhost:8100'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }*/
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
 app.use(router);
 
 app.use('/aviso', aviso);
@@ -38,15 +52,6 @@ app.use('/gestor', gestor);
 app.use('/categoria', categoria);
 app.use('/logro', logro);
 app.use('/mail', mailroutes);
-
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();ng
-});
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
