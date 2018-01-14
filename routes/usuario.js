9e3bf9930b6036e83f14de80345f517e165479f1
Roberto <roberto.arranz.93@gmail.com>
2018-01-14 17:03:15 +0100

    correct path to volume

--- routes/usuario.js
+++ routes/usuario.js
@@ -97,7 +97,7 @@ router.post('/image/:id', md_upload, function (req, res) {
 
     console.log(req.files);
     var file_path = req.files.image.path;
-    var file_split = file_path.split('\\');
+    var file_split = file_path.split('\/');
     var file_name = file_split[2];
     var ext_split = file_path.split('\.');
     var file_ext = ext_split[1];
