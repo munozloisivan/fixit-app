d8c74fa8955fd41bff3b4b06845c586d2ebbe245
Roberto <roberto.arranz.93@gmail.com>
2018-01-15 19:28:26 +0100

    detalles aviso, listado usuarios

---
+++ src/app/usuario-details/usuario-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsuarioDetailsComponent } from './usuario-details.component';
+
+describe('UsuarioDetailsComponent', () => {
+  let component: UsuarioDetailsComponent;
+  let fixture: ComponentFixture<UsuarioDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UsuarioDetailsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsuarioDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
