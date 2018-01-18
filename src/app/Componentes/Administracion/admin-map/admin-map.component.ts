a6cb207acf807c3ebb7d76440d1ecbc5f14d6160
Roberto <roberto.arranz.93@gmail.com>
2018-01-18 09:59:51 +0100

    visual upgrade table buttons, filters mapa

--- src/app/Componentes/Administracion/admin-map/admin-map.component.ts
+++ src/app/Componentes/Administracion/admin-map/admin-map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AvisoService} from "../../../Services/aviso.service";
 import {Router} from "@angular/router";
+import {CategoriaService} from "../../../Services/categoria.service";
 
 @Component({
   selector: 'app-admin-map',
@@ -10,15 +11,45 @@ import {Router} from "@angular/router";
 export class AdminMapComponent implements OnInit {
 
   avisos: any;
+  ciudad_filtrado: any;
+  tipo_filtrado: any;
+  categorias: any;
 
-  constructor(private avisoService: AvisoService, private router: Router) {
+  constructor(private avisoService: AvisoService, private categoriasService: CategoriaService, private router: Router) {
   }
 
   ngOnInit() {
     this.getAvisoList();
+    this.getCategorias();
   }
   getAvisoList() {
     this.avisoService.getAllAvisos().then((res) => {
+      this.avisos = res;
+      this.tipo_filtrado = null;
+      this.ciudad_filtrado = null;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  getCategorias() {
+    this.categoriasService.getAllCategorias().then((res) => {
+      this.categorias = res;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  getAvisosByTipo(tipo) {
+    this.avisoService.getAvisosByTipo(tipo).then(res => {
+      this.avisos = res;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  getAvisosByCiudad(ciudad) {
+    this.avisoService.getAvisosByCiudad(ciudad).then(res => {
       this.avisos = res;
     }, (err) => {
       console.log(err);
