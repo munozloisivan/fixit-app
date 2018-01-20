a5098e64ac90f17481331de2ce156580f4d7bf79
Dani <danimb93@gmail.com>
2018-01-20 19:42:12 +0100

    Page contacto final

--- src/app/Layouts/contacto/contacto.component.ts
+++ src/app/Layouts/contacto/contacto.component.ts
@@ -7,31 +7,11 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ContactoComponent implements OnInit {
-  messageToSend.email = this.emailC;
 
-  constructor() {
-  }
+  constructor() { }
 
   ngOnInit() {
     window.scrollTo(0, 0);
   }
-  function(user, done) {
-    var data = {
-      to: user.email,
-      from:user.email,
-      subject: user.,
-      context: {
-        url: 'http://localhost/reset_password?token=' ,
-        name: user.name.split(' ')[0]
-      },
-    };
 
-    smtpTransport.sendMail(data, function(err) {
-      if (!err) {
-        return res.json({ message: 'Kindly check your email for further instructions' });
-      } else {
-        return done(err);
-      }
-    });
-  }
 }
